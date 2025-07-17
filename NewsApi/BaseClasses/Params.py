61ad73ef868b77d9f18fc13d62dec188b52d53fb
C-Jurgensen <cadejurgensen@outlook.com>
2025-07-17 01:37:20 -0600

    Adding functionality for options checking.

--- NewsApi/BaseClasses/Params.py
+++ NewsApi/BaseClasses/Params.py
@@ -1,12 +1,30 @@
 from .Constraints import ParamConstraint
 
+from typing import Literal, Union
+
 __all__ = ["Param"]
+
+class _ParamChecker:
+
+    def __init__(self, options:set):
+        self.options = options
+
+    def __call__(self, param_val):
+        if not isinstance(param_val, (int, str)):
+            raise TypeError('You must use a custom check for checking literals other than integers and strings.')
+        if param_val not in self.options:
+            raise ValueError('Value %s was not found in the declared options available.' % param_val)
+
 
 class Param(ParamConstraint):
 
-    def __init__(self, name: str, /, options:list=None, **kwargs):
+    def __init__(self, name: str, *options: Union[str,int,any], checker = None, **kwargs):
         self.name = name
+        if len(options) != 0:
+            options = set(options)
+            self.checker = _ParamChecker(options) if checker is not None else checker
         super().__init__(**kwargs)
 
     def validate_param(self, value):
+        self(value)
         self(value)
